93e98a934c70f15f97ce8366ccd9e38555aa9c15
azyyz <rickyyz@outlook.com>
2024-02-23 14:19:20 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+
+    <h1>This is an about page</h1>
+    <el-input v-model="input" placeholder="Please input" />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script setup lang="ts">
+import { ref } from 'vue'
+const input = ref('啊P在家吗?')
+
+</script>
